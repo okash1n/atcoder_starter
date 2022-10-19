884360a6ddfb69a3b271200bdd34831da9e1825b
Shintaro Okamura <48118431+okash1n@users.noreply.github.com>
2022-10-20 01:54:04 +0900

    ex18

--- apg4b/ex18/main.cpp
+++ apg4b/ex18/main.cpp
@@ -1,8 +1,38 @@
-#include <iostream>
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main() {
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(M), B(M);
+    for (int i = 0; i < M; i++)
+    {
+        cin >> A.at(i) >> B.at(i);
+    }
 
+    // ここにプログラムを追記
+    // (ここで"試合結果の表"の2次元配列を宣言)
+    vector<vector<string>> result(N, vector<string>(N, "-"));
+
+    for (int i = 0; i < M; i++)
+    {
+        result.at(A.at(i) - 1).at(B.at(i) - 1) = "o";
+        result.at(B.at(i) - 1).at(A.at(i) - 1) = "x";
+    }
+
+    // resultを出力
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << result.at(i).at(j);
+            if (j != N - 1)
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 }
